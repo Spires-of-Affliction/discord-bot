843cb10d3a421dfeb15c8cce5182f2af3e83f5e7
Denis Onder <denis_onder@protonmail.com>
2020-06-09 06:36:23 +0200

    Alerting Discord members to storage changes

--- src/index.js
+++ src/index.js
@@ -4,23 +4,51 @@ const { Client } = require('discord.js');
 require('dotenv').config();
 
 const {
-  DEVELOPER_ROLE_ID,
-  GITHUB_WEBHOOK_NAME,
-  GITHUB_WEBHOOK_CHANNEL,
-  TOKEN,
+  DEVELOPER_ROLE_ID: developerRoleID,
+  GITHUB_WEBHOOK_NAME: githubWebhookName,
+  GITHUB_WEBHOOK_CHANNEL: githubWebhookChannel,
+  TOKEN: token,
   PORT: port,
+  GUILD_ID: guildID,
+  CHANNEL_ID: channelID,
 } = process.env;
 
 const server = new WebSocket.Server({ port });
 const client = new Client();
 
+console.log(`WebSocket server running!\nws://localhost:${port}`);
+
+const findChannel = () => {
+  const guild = client.guilds.cache.find(({ id }) => id === guildID);
+  if (!guild) throw Error('findChannel: Guild not found.');
+
+  const channel = guild.channels.cache.find(({ id }) => id === channelID);
+  if (!channel) throw Error('findChannel: Channel not found.');
+
+  return channel;
+};
+
+const handleSocketEvent = (msg) => {
+  switch (msg) {
+    case 'cloud::modified':
+      alertEveryone('Cloud storage has been updated!');
+      break;
+  }
+};
+
 server.on('connection', (ws) => {
-  ws.on('message', (msg) => console.log(msg));
+  ws.on('message', (msg) => handleSocketEvent(msg));
 });
+
+const alertEveryone = (msg) => {
+  const channel = findChannel();
+
+  channel.send(`@everyone\n${msg}`);
+};
 
 const alertDevelopers = ({ embeds, channel }) => {
   const [embed] = embeds;
-  const send = (msg) => channel.send(`<@&${DEVELOPER_ROLE_ID}>\n${msg}`);
+  const send = (msg) => channel.send(`<@&${developerRoleID}>\n${msg}`);
 
   if (embed.description.includes('Merge')) return send('Merge Alert!');
 
@@ -30,8 +58,8 @@ const alertDevelopers = ({ embeds, channel }) => {
 
 const handleMessage = (msg) => {
   if (
-    msg.channel.name === GITHUB_WEBHOOK_CHANNEL &&
-    msg.author.username === GITHUB_WEBHOOK_NAME
+    msg.channel.name === githubWebhookChannel &&
+    msg.author.username === githubWebhookName
   )
     alertDevelopers(msg);
 };
@@ -40,4 +68,4 @@ client.on('message', handleMessage);
 
 client.on('ready', () => console.log(client.user.tag));
 
-client.login(TOKEN);
+client.login(token);
